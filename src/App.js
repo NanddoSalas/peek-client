d7ea3305b6d349be0c2014e7f889838c49914200
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-10-31 00:20:16 -0700

    Create App component and style files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
