2d8928a936d847efd4a256a86f3eeef4c933348e
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-10-31 11:26:53 -0700

    update App component
     * add GlobalStyles
     * migrate to styled component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
+import GlobalStyles from './GlobalStyles';
 
-import Header from './components/Header';
+import { Header } from './components/Header/index';
 
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <GlobalStyles />
         <Header/>
       </React.Fragment>
     );
