182e8891394c85c1bdb62f6755d2611bc9773ddf
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-08 19:01:21 -0700

    Apply ApolloProvider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import client from './apollo';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
