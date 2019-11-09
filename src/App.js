e84d93dcc05d95d94582700ff3693866efba03f1
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-08 20:57:47 -0700

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
 import React from 'react';
 import GlobalStyles from './GlobalStyles';
+import { Router } from '@reach/router';
 
 import { Header } from './components/Header/index';
-import { CreateNoteFormContainer, NotesConatainer } from './AppStyle';
-import { CreateNoteForm } from './components/CreateNoteForm/index';
-import { Note } from './components/Note/index';
+import { AuthRoute } from './components/AuthRoute/index';
+import { Main } from './components/Main/index';
+import { LoginForm } from './components/LoginForm/index';
+import { SignupForm } from './components/SignupForm/index';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notes: [],
-    };
-  }
-  
-  handleSave = (title, text, uuid) => {
-    const { notes } = this.state;
-    notes.push({ title, text, uuid });
-    this.setState({ notes }); 
-  }
-
   render() {
     return (
       <React.Fragment>
         <GlobalStyles />
         <Header/>
-
-        <CreateNoteFormContainer>
-          <CreateNoteForm onSave={this.handleSave} />
-        </CreateNoteFormContainer>
-
-        <NotesConatainer>
-          {this.state.notes.map((note) => {
-            return <Note key={note.uuid} data={note} />
-          })}
-        </NotesConatainer>
-
+        <Router>
+          <AuthRoute path="/" component={Main} />
+          <LoginForm path="login" />
+          <SignupForm path="signup" />
+        </Router>
       </React.Fragment>
     );
   }
