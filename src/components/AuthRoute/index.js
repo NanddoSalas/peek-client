058283866143e5666956963eff56c702e2bc9c17
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-08 19:00:49 -0700

    Create AuthRoute component

---
+++ src/components/AuthRoute/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Redirect } from '@reach/router';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const ME = gql`
+{
+  me {
+    id
+  }
+}
+`;
+
+function AuthRoute(props) {
+  const { component: Component } = props;
+  const { loading, data } = useQuery(ME);
+
+  if (loading) return null
+
+  if (!data) return <Redirect from="/" to="login" />
+
+  if (!data.me) return <Redirect from="/" to="login" />
+
+  return <Component />
+}
+
+export { AuthRoute };
