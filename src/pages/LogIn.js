2324a2a29a30f3fdbbd652a9b4ad43c125858f77
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-12 00:09:18 -0700

    Create Signup and Login pages

---
+++ src/pages/LogIn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { FormContainer } from '../containers';
+import { LoginForm } from '../components/LoginForm';
+
+function LogIn() {
+  return (
+    <FormContainer>
+      <LoginForm />
+    </FormContainer>
+  );
+}
+
+export { LogIn };
---
+++ src/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { FormContainer } from '../containers';
+import { SignupForm } from '../components/SignupForm';
+
+function SignUp() {
+  return (
+    <FormContainer>
+      <SignupForm />
+    </FormContainer>
+  );
+}
+
+export { SignUp };
