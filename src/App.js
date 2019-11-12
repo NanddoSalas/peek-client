316df7be4c16aaeca8f372096f5049163e91fbef
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-12 00:10:02 -0700

    Update App component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Router } from '@reach/router';
 
+import { LogIn } from './pages/LogIn';
+import { SignUp } from './pages/SignUp';
+
 import { Header } from './components/Header/index';
 import { AuthRoute } from './components/AuthRoute/index';
 import { Main } from './components/Main/index';
-import { LoginForm } from './components/LoginForm/index';
-import { SignupForm } from './components/SignupForm/index';
 
 class App extends React.Component {
   render() {
@@ -16,8 +17,8 @@ class App extends React.Component {
         <Header />
         <Router>
           <AuthRoute path="/" component={Main} />
-          <LoginForm path="login" />
-          <SignupForm path="signup" />
+          <LogIn path="login" />
+          <SignUp path="signup" />
         </Router>
       </React.Fragment>
     );
