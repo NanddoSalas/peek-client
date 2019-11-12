4140ecfd7b37b1ff653f6f1e4e1d64260fcc927f
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-11 17:10:48 -0700

    Update TextFormField component

--- src/components/Fields.js
+++ src/components/Fields.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { getIn } from 'formik';
 import { TextField } from '@material-ui/core';
 
-const TextFormField = ({ field, form, ...props }) => {
+const TextFormField = ({ field, form, error, ...props }) => {
   const errorMessage = getIn(form.touched, field.name) && getIn(form.errors, field.name)
   return (
-    <TextField {...field} {...props} helperText={errorMessage} error={!!errorMessage} />
+    <TextField
+      {...field}
+      {...props}
+      helperText={errorMessage}
+      error={!!errorMessage || error}
+    />
   );
 }
 
