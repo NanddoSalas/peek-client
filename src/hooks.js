56db44bf862481915439f6078170e2ffc99b83d3
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-15 10:59:41 -0700

    Update useLogout hook
     * clear cache on each call

--- src/hooks.js
+++ src/hooks.js
@@ -2,10 +2,11 @@ import { useMutation } from '@apollo/react-hooks';
 import { LOGOUT } from './graphql/mutations';
 
 const useLogout = () => {
-  const [Logout] = useMutation(LOGOUT);
+  const [Logout, { client }] = useMutation(LOGOUT);
 
   const logout = async () => {
     await Logout();
+    client.resetStore();
   };
 
   return logout;
