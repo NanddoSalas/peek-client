3b9dbbe0b70094f750f7ee0628e67bb49da87e6c
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-14 23:33:13 -0700

    Create NoteList component

---
+++ src/components/NoteList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GETNOTES } from '../graphql/querys';
+
+import Masonry from 'react-masonry-component';
+import { Note } from './Note';
+
+function NoteList() {
+  const { data } = useQuery(GETNOTES);
+  console.log(data);
+  return (
+    <Masonry>
+      {data ? data.notes.map(({ title, text, id }) => (
+        <Note
+          data={{ title, text }}
+          key={id}
+        />
+      )) : null}
+    </Masonry>
+  );
+}
+
+export { NoteList };
