4844a9d23fb03d7dc318890101ca72e3ba25f794
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-17 23:20:43 -0700

    Update apollo config
     - Set up ws link

--- src/apollo.js
+++ src/apollo.js
@@ -1,21 +1,41 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
+import { WebSocketLink } from 'apollo-link-ws';
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/subscriptions',
+  options: {
+    reconnect: true,
+  },
+});
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/',
+  credentials: 'include',
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link: ApolloLink.from([
-    onError(({ graphQLErrors, networkError }) => {
-      console.log(graphQLErrors);
-      console.log(networkError);
-    }),
-    new HttpLink({
-      uri: 'http://localhost:4000/',
-      credentials: 'include',
-    }),
-  ]),
-  cache: new InMemoryCache(),
+  link,
+  cache,
 });
 
 export default client;
