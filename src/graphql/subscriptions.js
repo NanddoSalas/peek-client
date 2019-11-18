9efa3611cb1232982d1bb98dfefa0cb1c05f93a4
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-18 01:05:04 -0700

    Create onNoteUpdated and onNoteDeleted subs...

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -10,4 +10,22 @@ subscription onNoteAdded {
 }
 `;
 
-export { NOTES_SUBSCRIPTION };
+const NOTE_UPDATED = gql`
+subscription onNoteUpdated{
+  noteUpdated {
+    id
+    title
+    text
+  }
+}
+`;
+
+const NOTE_DELETED = gql`
+subscription onNoteDeleted{
+   noteDeleted{
+    id
+  }
+}
+`;
+
+export { NOTES_SUBSCRIPTION, NOTE_UPDATED, NOTE_DELETED };
