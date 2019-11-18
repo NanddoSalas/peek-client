d938207ef9765bc65b5126ad1bc42fb3e40804aa
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-11-17 20:54:26 -0700

    Update NoteList component
     - Add note update logic

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { GETNOTES } from '../graphql/querys';
-
+import { useUpdateNote } from '../hooks';
+import { NoteEditModalContainer } from '../containers';
 import Masonry from 'react-masonry-component';
+import Modal from '@material-ui/core/Modal';
 import { Note } from './Note';
+import { NoteEditModal } from './NoteEditModal';
 
 function NoteList() {
   const { data } = useQuery(GETNOTES);
+  const [open, setOpen] = useState(false);
+  const { modifiedNote, handleChange, performUpdate, setNote } = useUpdateNote(null);
+
+  const handleOpen = (note) => {
+    setNote(note);
+    setOpen(true);
+  };
+
+  const handleClose = async () => {
+    setOpen(false);
+    await performUpdate();
+    setNote(null);
+  };
+
   return (
-    <Masonry>
-      {data ? data.notes.map((note) => (
-        <Note
-          note={note}
-          key={note.id}
-        />
-      )) : null}
-    </Masonry>
+    <React.Fragment>
+
+      <Masonry>
+        {data ? data.notes.map((note) => (
+          <Note
+            note={note}
+            key={note.id}
+            onEdit={handleOpen}
+          />
+        )) : null}
+      </Masonry>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <NoteEditModalContainer>
+          <NoteEditModal note={modifiedNote} onChange={handleChange} close={handleClose} />
+        </NoteEditModalContainer>
+      </Modal>
+
+    </React.Fragment>
   );
 }
 
