f153511e465d5f6de8c0b25609e7538910bb3729
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-12-04 23:36:35 -0700

    Update apollo config

--- src/apollo.js
+++ src/apollo.js
@@ -6,21 +6,23 @@ import { split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { WebSocketLink } from 'apollo-link-ws';
 
-const wsLink = new WebSocketLink({
-  uri: process.env.REACT_APP_SUBSCRIPTIONS_URL || 'ws://localhost:4000/subscriptions',
+export const wsLink = new WebSocketLink({
+  uri:
+    process.env.REACT_APP_SUBSCRIPTIONS_URL ||
+    'ws://localhost:4000/subscriptions',
   options: {
-    reconnect: true,
+    reconnect: false,
   },
 });
 
-const httpLink = new HttpLink({
+export const httpLink = new HttpLink({
   uri: process.env.REACT_APP_API_URL || 'http://localhost:4000/',
   credentials: 'include',
 });
 
 // using the ability to split links, you can send data to each link
 // depending on what kind of operation is being sent
-const link = split(
+export const link = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
     return (
@@ -32,7 +34,7 @@ const link = split(
   httpLink,
 );
 
-const cache = new InMemoryCache();
+export const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link,
