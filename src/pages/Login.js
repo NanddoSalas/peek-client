0243e78833fdfeefce24c98203770626a438048f
Luis Fernando Cano Salas <f3rn4ndd0@gmail.com>
2019-12-05 22:23:26 -0700

    Add title to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Typography } from '@material-ui/core';
 import { FormContainer, FormPaper } from '../containers';
 import { GoogleButton } from '../components/GoogleButton';
 
@@ -7,6 +7,9 @@ function Login() {
   return (
     <FormContainer>
       <FormPaper>
+        <div style={{ padding: '16px 0' }}>
+          <Typography variant="h5">To start creating your notes:</Typography>
+        </div>
         <GoogleButton fullWidth />
       </FormPaper>
     </FormContainer>
